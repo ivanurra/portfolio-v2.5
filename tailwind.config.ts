7a577431a57f922111aeb70d49b73a15a0f035a8
Ivan Urra <ivan.urra.wd@gmail.com>
2024-03-27 07:41:18 +0100

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,42 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "background-secondary": "rgb(50, 58, 93)",
+        "font-color-primary": "rgb(227, 231, 239)",
+        "font-color-secondary": "rgb(183, 178, 200)",
+        "font-color-terciary": "rgb(132, 135, 141)",
+        "green-primary": "#1de49f",
+        "green-secondary": "#053625",
+        "navbar-color": "#151828",
+      },
+      spacing: {
+        "spacing-xxl": "700px",
+      },
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    fontSize: {
+      xxs: "0.5rem",
+      xs: "0.7rem",
+      sm: "0.8rem",
+      base: "1rem",
+      m: "1.1rem",
+      l: "1.2rem",
+      xl: "1.3rem",
+      xxl: "1.5rem",
+      "2xl": "1.7rem",
+      "3xl": "3rem",
+      "4xl": "4rem",
+      "5xl": "5rem",
+      "6xl": "6rem",
+      "7xl": "7rem",
+      "8xl": "8rem",
     },
   },
   plugins: [],
+  variants: {},
 };
 export default config;
